c3b2ebaeb0bc7bc4817b6a1f595eb389d07e94a6
leooliveiradev <leooliveiradev@gmail.com>
2018-10-28 22:06:01 -0300

    This pull request is related with #12 and 13# issues.

--- src/js/components/App/index.js
+++ src/js/components/App/index.js
@@ -2,19 +2,43 @@ import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import ThemeProvider from 'app/js/common/ThemeProvider';
 import GlobalStyle from 'app/theme/global';
-import { fromTheme } from 'app/js/utils';
+import List from 'app/js/common/List';
+import TransactionItem from 'app/js/components/TransactionItem';
 
-const Title = styled.h1`
-  color: ${fromTheme('color.gray')}; 
+const transactions = [
+  {
+    uuid: 'sda56as56s5a',
+    description: 'habibs',
+    value: '50.00',
+    dateOfTransaction: 'Fri Sep 25 2018 09:18:04',
+    type: 'credit',
+    status: 'pending',
+  },
+  {
+    uuid: 'opop54545',
+    description: 'outback',
+    value: '200.00',
+    dateOfTransaction: 'Fri Oct 25 2018 09:18:04',
+    type: 'debit',
+    status: 'success',
+  },
+];
+
+const ListTransactions = styled.li`
+  display: flex;
+  width: 50%;
 `;
 
 const App = () => (
   <ThemeProvider>
     <Fragment>
       <GlobalStyle />
-      <Title>
-        Title
-      </Title>
+      <ListTransactions>
+        <List
+          items={transactions}
+          ItemComponent={TransactionItem}
+        />
+      </ListTransactions>
     </Fragment>
   </ThemeProvider>
 );
