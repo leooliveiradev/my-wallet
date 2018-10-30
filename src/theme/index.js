05f6b4a45ad8bce507304c53dddfcbbb1c561597
leooliveiradev <leooliveiradev@gmail.com>
2018-10-29 21:43:48 -0300

    Create card Amount
    * create card base component
    * create snapshot test
    * install react-test-renderer
    * create variables

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,6 +1,38 @@
 export default {
   color: {
-    gray: '#ccc',
+    primary: '#8200ff',
+    secondary: '#fff',
+
+    text: '#151515',
+    textLight: '#c6c9d9',
+
     white: '#fff',
+    black: '#151515',
+    grey: '#c6c9d9',
+    gray: '#9a9a9a',
+
+    shadow: '',
+
+    success: '',
+    pending: '',
+  },
+  fontStyle: {
+    primary: 'Roboto',
+    secondary: 'Sans Serif',
+  },
+  fontSize: {
+    small: '1rem',
+    medium: '1.5rem',
+    big: '2rem',
+  },
+  fontWeight: {
+    regular: 400,
+    semibold: 500,
+    bold: 700,
+  },
+  breakpoints: {
+    sm: '',
+    md: '',
+    lg: '',
   },
 };
